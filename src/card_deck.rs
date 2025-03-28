91ee0aafcc020e517a5ab83d99f97d9e12d7dd1f
Efi Kay <efikay@icloud.com>
2025-03-28 22:05:54 +0300

    Add CardDeck struct

---
+++ src/card_deck.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+use rand::prelude::*;
+
+use strum::IntoEnumIterator;
+
+use crate::{
+    card::Card, core_types::{GameMode, Suit}, stack::Stack
+};
+
+struct CardDeck {
+    remaining_cards: Stack<Card>,
+}
+
+impl CardDeck {
+    pub fn new(game_mode: GameMode) -> Self {
+        let full_sequences_per_suit = match game_mode {
+            GameMode::OneSuit => 8,
+            GameMode::TwoSuits => 4,
+            GameMode::FourSuits => 2,
+        };
+        let suits = match game_mode {
+            GameMode::OneSuit => vec![Suit::Spades],
+            GameMode::TwoSuits => vec![Suit::Spades, Suit::Hearts],
+            GameMode::FourSuits => Suit::iter().collect(),
+        };
+
+        let mut full_deck = make_deck_of(suits, full_sequences_per_suit);
+        shuffle_cards(&mut full_deck);
+
+        Self {
+            remaining_cards: Stack::from_iter(full_deck),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.remaining_cards.len()
+    }
+
+    pub fn take_card(&mut self) -> Option<Card> {
+        self.remaining_cards.pop()
+    }
+}
+
+fn shuffle_cards(cards: &mut Vec<Card>) {
+    let mut rng = rand::rng();
+
+    cards.shuffle(&mut rng);
+}
+
+fn make_deck_of(suits: Vec<Suit>, full_sequences_per_suit: usize) -> Vec<Card> {
+    suits
+        .iter()
+        .map(|suit| {
+            let full_sequence = Card::make_full_sequence_of(*suit);
+
+            std::iter::repeat(full_sequence)
+                .take(full_sequences_per_suit)
+                .flatten()
+        })
+        .flatten()
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod card;
 mod card_sequence;
 mod card_pile;
 mod game_tableau;
+mod card_deck;
 
 fn main() {
     println!("Hello, world!");
