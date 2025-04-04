b9f94eb84b77e01291885aff69b8a003e7ae0a49
Efi Kay <efikay@icloud.com>
2025-04-05 00:33:39 +0300

    Fix GameCursor bugs, Add some tests for it

--- src/app/game_window/game_cursor.rs
+++ src/app/game_window/game_cursor.rs
@@ -2,6 +2,7 @@
 
 use crate::game::core::PILES_AMOUNT;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum GameCursorMode {
     /// Cursor used to select a card
     ///
@@ -30,6 +31,7 @@ pub enum GameCursorMode {
     PileSelect([bool; PILES_AMOUNT]),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct GameCursor {
     mode: Option<GameCursorMode>,
 
@@ -136,11 +138,11 @@ impl GameCursor {
                         // Assuming that there were no valid piles to place cursor on. So nowhere to move_right in that case
                         assert!(pile_lengths.iter().all(|len| *len == 0))
                     }
-                    Some(max_index) if max_index == PILES_AMOUNT => {
+                    Some(max_index) if max_index == (PILES_AMOUNT - 1) => {
                         // TODO: try moving to most-left?
                     }
                     Some(pile_index) => {
-                        for pile_index_candidate in pile_index..PILES_AMOUNT {
+                        for pile_index_candidate in (pile_index + 1)..PILES_AMOUNT {
                             let is_valid = pile_lengths[pile_index_candidate] > 0;
                             if is_valid {
                                 if let Some(prev_card_index) = self.card_index {
@@ -166,11 +168,11 @@ impl GameCursor {
                     None => {
                         assert!(pile_filters.iter().all(|filter| !filter))
                     }
-                    Some(max_index) if max_index == PILES_AMOUNT => {
+                    Some(max_index) if max_index == (PILES_AMOUNT - 1) => {
                         // TODO: try moving to most-left?
                     }
                     Some(pile_index) => {
-                        for pile_index_candidate in pile_index..PILES_AMOUNT {
+                        for pile_index_candidate in (pile_index + 1)..PILES_AMOUNT {
                             let is_valid = pile_filters[pile_index_candidate];
                             if is_valid {
                                 self.pile_index = Some(pile_index_candidate);
@@ -218,9 +220,7 @@ impl GameCursor {
                 match self.pile_index {
                     Some(pile_index) => {
                         match self.card_index {
-                            Some(max_card_index)
-                                if max_card_index == pile_lengths[pile_index] =>
-                            {
+                            Some(max_card_index) if max_card_index == pile_lengths[pile_index] => {
                                 // No-no-no...
                                 // TODO: Try to "teleport" to up-most card?
                             }
@@ -301,7 +301,7 @@ impl GameCursor {
                             }
                         }
 
-                        self.pile_index = None;
+                        self.card_index = Some(0);
                         return;
                     }
                     None => {
@@ -317,5 +317,208 @@ impl GameCursor {
                 // Leave as is
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_be_empty_initially() {
+        let cursor = GameCursor::new();
+
+        assert!(cursor.mode.is_none());
+        assert!(cursor.pile_index.is_none());
+        assert!(cursor.card_index.is_none());
+    }
+
+    #[test]
+    fn should_set_up_correctly() {
+        {
+            let pile_lengths: [usize; PILES_AMOUNT] = [1, 1, 1, 1];
+            let mut cursor = GameCursor::new();
+
+            cursor.set_for_card_selection(pile_lengths);
+            assert_eq!(cursor.mode, Some(GameCursorMode::CardSelect(pile_lengths)));
+            assert_eq!(cursor.pile_index, Some(0));
+            assert_eq!(cursor.card_index, Some(0));
+        }
+
+        let pile_filters = [true, true, true, true];
+        let mut cursor = GameCursor::new();
+
+        cursor.set_for_pile_selection(pile_filters);
+        assert_eq!(cursor.mode, Some(GameCursorMode::PileSelect(pile_filters)));
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, None); // Should stay untouched
+    }
+
+    #[test]
+    fn should_move_right_left_correctly() {
+        let pile_lengths: [usize; PILES_AMOUNT] = [1, 1, 1, 1];
+        let pile_filters = pile_lengths.map(|p| p > 0);
+
+        let mut cursor = GameCursor::new();
+
+        cursor.set_for_card_selection(pile_lengths);
+
+        // Let's try to go left once
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0));
+
+        // Go spam right button
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(1));
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(2));
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+
+        // And now we fly to the left
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(2));
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(1));
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+
+        // And now we're trying pile mode
+        cursor.set_for_pile_selection(pile_filters);
+        assert_eq!(cursor.mode, Some(GameCursorMode::PileSelect(pile_filters)));
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0)); // Should stay untouched
+
+        // Go spam right button AGAIN
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(1));
+        assert_eq!(cursor.card_index, Some(0)); // Still untouched
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(2));
+        assert_eq!(cursor.card_index, Some(0)); // And even now
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+        assert_eq!(cursor.card_index, Some(0)); // Basically all the time in PileMode
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+        assert_eq!(cursor.card_index, Some(0)); // Index shall stay the same
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(3));
+        assert_eq!(cursor.card_index, Some(0)); // ... You know
+
+        // LAST TIME. WE GO LEFT
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(2));
+        assert_eq!(cursor.card_index, Some(0)); // Yes I'm schizo maybe ...
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(1));
+        assert_eq!(cursor.card_index, Some(0)); // Did I ever tell you..
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0)); // the definition...
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0)); // OF INSANITY? ...
+        cursor.move_left();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0)); // 400+ lines already? ...
+    }
+
+    #[test]
+    fn should_move_up_down_correctly() {
+        let pile_lengths: [usize; PILES_AMOUNT] = [4, 0, 0, 0];
+        let pile_filters = pile_lengths.map(|p| p > 0);
+
+        let mut cursor = GameCursor::new();
+
+        cursor.set_for_card_selection(pile_lengths);
+
+        // Let's press UP once
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0));
+
+        // Go down-down the dream
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(1));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(2));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(3));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(4));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(4));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(4));
+        assert_eq!(cursor.pile_index, Some(0));
+
+        // Stairwayyy to heaven
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(3));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(2));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(1));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(0));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(0));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_up();
+        assert_eq!(cursor.card_index, Some(0));
+        assert_eq!(cursor.pile_index, Some(0));
+
+        // Very funny... It cannot move up-down but let's test it out!
+        cursor.set_for_pile_selection(pile_filters);
+
+        // Go spam UP button once
+        cursor.move_right();
+        assert_eq!(cursor.pile_index, Some(0));
+        assert_eq!(cursor.card_index, Some(0));
+
+        // Go down let's say... twice
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(0));
+        assert_eq!(cursor.pile_index, Some(0));
+        cursor.move_down();
+        assert_eq!(cursor.card_index, Some(0));
+        assert_eq!(cursor.pile_index, Some(0));
+    }
+
+    #[test]
+    #[ignore]
+    fn should_jump_over_empty_piles() {
+        todo!("Tricky functionality and should be tested");
+    }
+
+    #[test]
+    #[ignore]
+    fn should_go_square_correctly() {
+        todo!("Tricky functionality and should be tested");
+    }
+
+    #[test]
+    #[ignore]
+    fn should_go_diagonal_correctly() {
+        todo!("Tricky functionality and should be tested");
     }
 }
